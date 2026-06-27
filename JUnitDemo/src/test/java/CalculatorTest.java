7dd41b04beb4e1c9a30bb45b3d76868f2e076a0e
Paalpraveen01 <paalpraveen121@gmail.com>
2026-06-27 13:43:06 +0530

    Remove JUnitDemo folder from repository

--- JUnitDemo/src/test/java/CalculatorTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-public class CalculatorTest {
-@Test
-public void TenPlusTwentyEqualsThirty() {
-	Calculator calculator=new Calculator();
-	assertEquals(30,calculator.add(10,20));
-}
-}
